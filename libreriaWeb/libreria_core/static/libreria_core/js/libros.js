ad0b66c419c312e8496c520e062493a39e423041
Vict-vargass <vict.vargass@duocuc.cl>
2022-04-19 23:05:01 -0400

    url + imagenes

--- libreriaWeb/libreria_core/static/libreria_core/js/libros.js
+++ libreriaWeb/libreria_core/static/libreria_core/js/libros.js
@@ -4,16 +4,18 @@ $(document).ready(function(){
     contenedor.empty();
 
     $.get({
-        url: 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bibloteca/',
+        url: 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/libro/',
 
         success: function(respuestaOK){
             $.each(respuestaOK.items, function(indice, album){
-                contenedor.append("<div class='card'>" +
-                "<div class='card-body'>"+
-                "<h5 class='card-title'>" + album.nombre + "</h5>" +
-                "<p class='card-text'><b>Precio: </b>$" + album.precio.toLocaleString("es-CL") + "<br>"+"Stock: "+album.stock+"</p>"+
+                console.log(album);
+                contenedor.append("<div class='card dec-hov shadow'>" +
+                "<img src='" + album.portada + "' class='card-img-top max-tam-img' alt='"+ "'>"+
+                "<div class='card-body shadow'>"+
+                "<h5 class='card-title text-center'>" + album.nombre + "</h5>" +
+                "<p class='card-text text-center'><b>Precio: </b>$" + album.precio.toLocaleString("es-CL") + "<br>"+"Stock: "+album.stock+"</p>"+
             "</div></div>")
-            })
+            })   
         },
         error: function(respuestaError){
             console.error(respuestaError);
