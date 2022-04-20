a35f58fab334c43162e193baf22cbf7d47fce134
Vict-vargass <vict.vargass@duocuc.cl>
2022-04-19 23:05:28 -0400

    url para post, debe cambiar

--- libreriaWeb/libreria_core/static/libreria_core/js/registro.js
+++ libreriaWeb/libreria_core/static/libreria_core/js/registro.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
     var stockLibro=document.getElementById('stockLibro');
     var portada= document.getElementById('portada');
     var url = 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bibloteca/';
-
+    var url2 = 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/libro/';
+   
     $('#registroBD').click(function(){
     var data = {
         "codigo": ""+ codigoLibro.value.toUpperCase(),
@@ -15,14 +16,22 @@ $(document).ready(function(){
         "stock" : Number(stockLibro.value),
     };
 
-    var urlGet = 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bibloteca/'+codigoLibro.value.toUpperCase();
+    var data2 = {
+        "codigo": ""+ codigoLibro.value.toUpperCase(),
+        "nombre" : ""+nombreLibro.value,
+        "precio" : Number(precioLibro.value),
+        "stock" : Number(stockLibro.value),
+        "portada" : portada.value
+    };
+
+    var urlGet = 'https://gaff8f4466ad45b-library01db.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/libro/'+codigoLibro.value.toUpperCase();
 
 
     $.get({
         urlGet,
         error: function(status){
             if (status=404){
-                $.post(url, data, function(){
+                $.post(url2, data2, function(){
                     console.clear()
                 });
             }
